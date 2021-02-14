aa6186d8985b32f65c4e3828742aba80f3f095ec
denisbalazic <denisbalazic42@gmail.com>
2021-02-14 14:32:22 +0100

    feat: Add icons to answer

--- components/Answer.js
+++ components/Answer.js
@@ -1,16 +1,19 @@
 import React from "react";
 import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
 
 const Answer = ({ answer, handleAnswerShowed, handleAnswer }) => {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>{answer}</Text>
-      <View>
-        <TouchableOpacity style={styles.touchable} onPress={() => handleAnswer(true)}>
-          <Text style={styles.touchableText}>Correct</Text>
+      <View style={styles.checkmarks}>
+        <TouchableOpacity style={styles.checkmarkTouchable} onPress={() => handleAnswer(true)}>
+          <AntDesign name="checksquareo" size={60} color="black" />
+          <Text style={styles.checkmarkText}>Correct</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.touchable} onPress={() => handleAnswer(false)}>
-          <Text style={styles.touchableText}>Incorrect</Text>
+        <TouchableOpacity style={styles.checkmarkTouchable} onPress={() => handleAnswer(false)}>
+          <AntDesign name="closesquareo" size={60} color="black" />
+          <Text style={styles.checkmarkText}>Incorrect</Text>
         </TouchableOpacity>
       </View>
 
@@ -28,10 +31,25 @@ const styles = StyleSheet.create({
     margin: 10,
   },
   text: {
+    alignSelf: "center",
     marginTop: 30,
     marginBottom: 30,
     fontSize: 27,
     color: "#6b705c",
+  },
+  checkmarks: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+  checkmarkTouchable: {
+    alignItems: "center",
+    marginTop: 12,
+    padding: 6,
+  },
+  checkmarkText: {
+    alignSelf: "center",
+    fontSize: 20,
+    color: "black",
   },
   touchable: {
     marginTop: 12,
